e43219104eb3610bcb11bd32e53ae049e97a1745
zyugat <zyugatcn@gmail.com>
2021-12-26 19:55:43 +0800

    2.0.0-beta.31

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from 'D:/Project/ProjectCode/real/zyugat-vuepress/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
